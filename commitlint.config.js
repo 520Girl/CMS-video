e7aae1b9991af1bff662f37098d2d4814b8daa7a
520Girl <815842080@qq.com>
2025-02-20 01:04:09 +0800

    init: 初始化uniapp

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'init',
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+      ],
+    ],
+  },
+};
---
+++ packages/shared/src/index.ts
@@ -0,0 +1,8 @@
+// 共享组件
+export * from './components';
+
+// 共享工具函数
+export * from './utils';
+
+// 共享类型定义
+export * from './types';
